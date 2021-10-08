5108793bfc83ffead7f85b21188f25f007ba8648
kiririnyo <meilvivid@gmail.com>
2021-10-08 17:13:53 +0900

    filter, flatMapを追加

--- src/main/java/local/kiririnyo/stream/StreamPerform.java
+++ src/main/java/local/kiririnyo/stream/StreamPerform.java
@@ -1,14 +1,16 @@
 package local.kiririnyo.stream;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamPerform {
 
-    private static final List<StreamParamBean> PARAM_LIST = List.of(
+    private static final List<StreamParamBean> NIJIGAKU_LIST = List.of(
             new StreamParamBean("上原歩夢", 2),
             new StreamParamBean("中須かすみ", 1),
             new StreamParamBean("桜坂しずく", 1),
@@ -23,9 +25,23 @@ public class StreamPerform {
             new StreamParamBean("鐘 嵐珠", 2)
     );
 
+    private static final List<StreamParamBean> AQOURS_LIST = List.of(
+            new StreamParamBean("高海千歌", 2),
+            new StreamParamBean("桜内梨子", 2),
+            new StreamParamBean("松浦果南", 3),
+            new StreamParamBean("黒澤ダイヤ", 3),
+            new StreamParamBean("渡辺曜", 2),
+            new StreamParamBean("津島善子", 1),
+            new StreamParamBean("国木田花丸", 1),
+            new StreamParamBean("小原鞠莉", 3),
+            new StreamParamBean("黒澤ルビィ", 1)
+    );
+
     public static void main(String[] args) {
         forEach();
         map();
+        flatMap();
+        filter();
         parallelStream();
     }
 
@@ -33,24 +49,55 @@ public class StreamPerform {
         // リストの要素を見て何か処理をしたいときに使う
         // 返り値はない
         System.out.println("-- forEach --");
-        PARAM_LIST.forEach(param -> System.out.println(param.getName()));
+        NIJIGAKU_LIST.forEach(idle -> System.out.println(idle.getName()));
 
         // forEachOrderedは並列処理にした場合も順番を担保する
         System.out.println("-- parallel forEach --");
-        PARAM_LIST.parallelStream().forEach(param -> System.out.println(param.getName()));
+        NIJIGAKU_LIST.parallelStream().forEach(idle -> System.out.println(idle.getName()));
         System.out.println("-- parallel forEachOrdered --");
-        PARAM_LIST.parallelStream().forEachOrdered(param -> System.out.println(param.getName()));
+        NIJIGAKU_LIST.parallelStream().forEachOrdered(idle -> System.out.println(idle.getName()));
     }
 
     private static void map() {
-        // リストの要素に対してなにかしら処理をし、新たなstreamを生成します
-        Stream<String> st = PARAM_LIST.stream().map(StreamParamBean::getName);
+        // リストの要素に対して型の変換を行い、新たなstreamを生成する
+        final Stream<String> st = NIJIGAKU_LIST.stream().map(StreamParamBean::getName);
 
-        // 生成したstreamに対してcollect等の操作をし、新たな配列や文字列を生成する使い方が多いです
+        // 生成したstreamに対してcollect等の操作をし、新たな配列や文字列を生成する使い方が多い
         final List<String> nameList = st.collect(Collectors.toList());
 
-        System.out.println("-- map and collect --");
+        System.out.println("-- map --");
         System.out.println(nameList);
+    }
+
+    private static void flatMap() {
+
+        // 型の変換を行う点ではmapと同じだが、こちらはstreamへの変換
+        // mapが1:1ならこちらは1:Nの変換となる
+        System.out.println("-- flatMap --");
+
+        // 2次元配列が生成されるわけではなくflatになる
+        // 例えば2次元配列をflatMapでstreamに変換すると、1次元配列にまとまる
+        final List<List<StreamParamBean>> schoolIdleList = List.of(AQOURS_LIST, NIJIGAKU_LIST);
+        final Stream<StreamParamBean> st = schoolIdleList.stream().flatMap(Collection::stream);
+        final List<String> nameList = st.map(StreamParamBean::getName).collect(Collectors.toList());
+        System.out.println(nameList);
+
+        // 1次元配列に他の要素を加えた1次元配列にすることも可能
+        final List<Object> mixedList = NIJIGAKU_LIST.stream()
+                .flatMap(idle -> Stream.of(idle.getName(), idle.getSchoolYear()))
+                .collect(Collectors.toList());
+        System.out.println(mixedList);
+    }
+
+    private static void filter() {
+        // 指定した条件に合うものだけを残したstreamを生成する
+        final Stream<StreamParamBean> st = NIJIGAKU_LIST.stream().filter(idle -> idle.getSchoolYear() == 3);
+
+        // 生成したstreamに対してmapを続けたりcollectで新たな配列などを生成する使い方が多い
+        final List<String> thirdGradeMemberNameList = st.map(StreamParamBean::getName).collect(Collectors.toList());
+
+        System.out.println("-- filter --");
+        System.out.println(thirdGradeMemberNameList);
     }
 
     private static void parallelStream() {
